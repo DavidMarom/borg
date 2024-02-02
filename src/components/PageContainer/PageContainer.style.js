b58df82f0f161f16ecbe0bd1a5301883000882ca
David Marom <davidmarom.pro@gmail.com>
2024-02-02 20:46:06 +0200

    wip

--- src/components/PageContainer/PageContainer.style.js
+++ src/components/PageContainer/PageContainer.style.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100vh;
   width: 100%;
-  background-image: url('bg02.jpg');
+  background-image: url('bg03.jpg');
   background-size: cover;
   position: absolute;
 
